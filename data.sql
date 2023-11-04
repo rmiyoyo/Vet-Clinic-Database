63915417f250341aaebdc5bc8e955a30f33e094c
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-04 21:43:22 +0300

    Insert data into animals table

--- data.sql
+++ data.sql
@@ -13,3 +13,31 @@ VALUES ('Pikachu', '2021-01-07', 15.04, FALSE, 1);
 -- Insert data for Devimon
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Devimon', '2017-05-12', 11, TRUE, 5);
+
+-- Insert data for Charmander
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Charmander', '2020-02-08', -11, FALSE, 0, 'Fire');
+
+-- Insert data for Plantmon
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Plantmon', '2021-11-15', -5.7, TRUE, 2, 'Plant');
+
+-- Insert data for Squirtle
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Squirtle', '1993-04-02', -12.13, FALSE, 3, 'Water');
+
+-- Insert data for Angemon
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Angemon', '2005-06-12', -45, TRUE, 1, 'Angel');
+
+-- Insert data for Boarmon
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Boarmon', '2005-06-07', 20.4, TRUE, 7, 'Beast');
+
+-- Insert data for Blossom
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Blossom', '1998-10-13', 17, TRUE, 3, 'Plant');
+
+-- Insert data for Ditto
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Ditto', '2022-05-14', 22, TRUE, 4, 'Normal');
