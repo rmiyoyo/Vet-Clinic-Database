7fbee1fe59c148e46252647c5882022f23135282
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-04 22:48:30 +0300

    Create owners, species tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -14,3 +14,41 @@ CREATE TABLE animals (
 -- Add the "species" column of type string to the "animals" table.
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+    id serial PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id serial PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- Remove the existing "species" column
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Add a new column "species_id" as a foreign key referencing the "species" table
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER;
+
+-- Add a new column "owner_id" as a foreign key referencing the "owners" table
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER;
+
+-- Set "id" as an auto-incremented primary key
+ALTER TABLE animals
+ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
+
+-- Add foreign key constraints
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
